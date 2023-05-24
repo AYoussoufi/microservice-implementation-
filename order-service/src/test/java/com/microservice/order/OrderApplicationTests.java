9928f85448ebe7f6febddaaa92cbe282ef3bd465
Akram <akram.lyoussoufi@gmail.com>
2023-05-24 10:48:32 +0100

    PUSH

--- order-service/src/test/java/com/microservice/order/OrderApplicationTests.java
+++ order-service/src/test/java/com/microservice/order/OrderApplicationTests.java
@@ -5,8 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 class OrderApplicationTests {
